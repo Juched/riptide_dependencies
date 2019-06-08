b0fbb808f0f7989579944a554200436d5c613fa1
juched <prth.2000@gmail.com>
2019-06-07 22:59:59 -0500

    updated update_rospkgs to actually execute

--- update_rospkgs.sh
+++ update_rospkgs.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [[ $UID == 0 ]]; then
-	echo "Please do not run update_rospkgs.sh as root"
-	exit 1
-fi
+
 
 echo "SUBMODULE: imu_3dm_gx4"
 cd imu_3dm_gx4/
@@ -20,4 +17,4 @@ cd ..
 
 echo ""
 cd ..
-sudo su -c src/installation/install_rospkgs.sh root
+src/installation/install_rospkgs.sh
