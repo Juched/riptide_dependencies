6e6d0d89b958a56bee528427afd1f4654e84b5fc
juched <prth.2000@gmail.com>
2019-06-08 16:34:46 -0500

    trial for camera things

--- xfer-riptide.sh
+++
@@ -1,2 +0,0 @@
-~/osu-uwrt/riptide_software/src/riptide_utilities/dates_scripts/date_set-riptide.sh
-rsync -vrzc --delete --exclude=".*" --exclude=".*/" ~/osu-uwrt/riptide_dependencies/src ros@riptide:~/osu-uwrt/riptide_dependencies
