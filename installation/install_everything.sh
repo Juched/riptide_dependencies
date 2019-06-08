9891e2c81bd82358f8e7b45cd47b4b01fff4fb38
juched <prth.2000@gmail.com>
2019-06-07 21:33:23 -0500

    more fixes for docker

--- installation/install_everything.sh
+++ installation/install_everything.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-install_ros_kinetic.sh
-../update_rospkgs.sh
-install_eigen.sh
-install_ceres.sh
-install_acoustics.sh
+~/osu-uwrt/riptide_dependencies/src/installation/install_ros_kinetic.sh
+~/osu-uwrt/riptide_dependencies/src/update_rospkgs.sh
+~/osu-uwrt/riptide_dependencies/src/installation/install_eigen.sh
+~/osu-uwrt/riptide_dependencies/src/installation/install_ceres.sh
+~/osu-uwrt/riptide_dependencies/src/installation/install_acoustics.sh
 
 echo "Installed all Riptide Dependencies"
